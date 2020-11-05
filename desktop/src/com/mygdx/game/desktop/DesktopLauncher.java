2a82bca3060b7de8a38f230b252da3f3cae37da3
java3000@bitbucket.org <lexa709@mail.ru>
2020-11-06 01:39:00 +0300

    lesson1

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,10 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
+		//config.width = 1280;
+		//config.height = 720;
+		config.width = 500;
+		config.height = 500;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
